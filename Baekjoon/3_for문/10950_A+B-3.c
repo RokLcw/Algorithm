e41e79e2229b691a0ca2e534f34c93cf468e1208
RokLcw <lcwlyw@naver.com>
2021-01-08 14:36:24 +0900

    Baekjoon Algorithm

---
+++ Baekjoon/3_for문/10950_A+B-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int T;
+	int num1, num2;
+
+	scanf("%d", &T);
+
+	for (int i=0; i<T; i++)
+	{
+		scanf("%d %d", &num1, &num2);
+
+		printf("%d\n", num1 + num2);
+	}
+}
